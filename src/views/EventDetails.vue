9ecedcc13458d1da0fd1791e44ac106e7336a9e6
Daniels-not <ramydcampusanp@hotmail.com>
2021-07-30 13:03:56 -0400

    first commit

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="event">
+        <h1> 👀 {{ event.title }}</h1>
+        <p>🐱‍🏍 {{ event.time}} on {{ event.date }} @{{ event.location }}</p>
+        <p>🐱‍💻 {{ event.description }}</p>
+    </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+
+export default {
+    props: ['id'],
+    data(){
+        return{
+            event: null
+        }
+    },
+    created() {
+    EventService.getEvent(this.id)
+        .then(response => {
+            this.event = response.data
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+}
+</script>
